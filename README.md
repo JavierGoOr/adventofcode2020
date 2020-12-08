7c59a16e27bc491060d6a2f04778a09d0181fe42
javier <fj.gonzalez.ortega@gmail.com>
2020-12-08 10:32:05 +0100

    Changes on day 2 solution

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # adventofcode2020
 Advent of Code 2020
+
+https://adventofcode.com/2020/
+
+Using Bash only!
--- day2-first/solution.sh
+++ day2-first/solution.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 while IFS= read -r line
 do
-  REGEX=$(echo "$line" | awk -F'[ \-:]' '{print $3"{"$1","$2"}"}') 
-  RESULT=$(echo "$line" | awk -F'[ \-:]' '{print $5}' | grep -E $REGEX)
-  if [ ! -z "$RESULT" ]
+  MIN=$(echo "$line" | awk -F'[ \-:]' '{print $1}') 
+  MAX=$(echo "$line" | awk -F'[ \-:]' '{print $2}')  
+  CHAR=$(echo "$line" | awk -F'[ \-:]' '{print $3}')  
+  RESULT=$(echo "$line" | awk -F'[ \-:]' '{print $5}' | grep -o . | grep "$CHAR" | wc -l)
+  if [ "$MIN" -le "$RESULT" ] && [ "$RESULT" -le "$MAX" ]
   then
-    echo "$line"
+    echo "$line - $RESULT"
   fi
 done < input
